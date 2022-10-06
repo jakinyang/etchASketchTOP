bf405f69589593e015b40d9558d135e794bccb22
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-05 18:23:37 -0700

    Adds conditional prompt
    While loop was added to ensure user number choice is an integer and equal-to or less than 100.

--- etch.js
+++ etch.js
@@ -1,9 +1,9 @@
 /* Prompt for grid size */
-let answer = parseInt(window.prompt('Enter grid size up to 100:'))
+let answer = parseInt(window.prompt('Enter grid size up to 100:'));
 
-if (answer > 100) {
-  parseInt(window.prompt('Grid size must be less than or equal to 100:'))
-}
+while (answer > 100) {
+  answer = parseInt(window.prompt('Grid size must be less than or equal to 100:'));
+};
 
 console.log(answer);
 /* Setting up variable for DOM */
