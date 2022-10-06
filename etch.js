13635dbf1f56c93df95f52f7d2bedb2c2f5668b3
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-06 13:03:40 -0700

    Adds grid generation functionality
    Adding grid generation function with the appropriate styling and with basic hover effects.

--- etch.js
+++ etch.js
@@ -5,5 +5,47 @@ while (answer > 100) {
   answer = parseInt(window.prompt('Grid size must be less than or equal to 100:'));
 };
 
-console.log(answer);
+// console.log(answer);
+
 /* Setting up variable for DOM */
+const container = document.querySelector('#gridContainer');
+
+// Variable for equal w/h distribution
+let dimension = parseFloat(Math.sqrt(95 * 95 / answer));
+console.log(dimension);
+
+// Function to append no. of squares based on user input
+
+const appendDiv = () => {
+  const newDiv = document.createElement('div');
+  newDiv.className = 'gridDiv';
+  newDiv.style.width = `${dimension}vw`;
+  newDiv.style.height = `${dimension}vw`;
+  newDiv.style.backgroundColor = 'red';
+  newDiv.style.margin = '0.1vw';
+  newDiv.style.border = 'white';
+  container.appendChild(newDiv);
+  console.log(newDiv.style.width);
+}
+
+// Will only run if an input given to the prompt
+
+if (answer) {
+  for (let i = 0; i < answer; i++) {
+    appendDiv();
+  }
+}
+
+/* Hover effect */
+const gridDiv = document.querySelectorAll('gridDiv');
+
+const glow = (e) => {
+  console.log(e);
+  e.target.style.backgroundColor = 'blue';
+}
+
+gridDiv.forEach(div => {
+  div.addEventListener('mouseover', glow);
+})
+
+
